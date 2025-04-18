46555a1993d7a683d0076417c3577a7fb4135643
Claudel <mathys.claudel@etu.sorbonne-universite.fr>
2025-04-18 21:07:12 +0200

    test branch 2

--- src/test.cpp
+++ src/test.cpp
@@ -3,5 +3,7 @@
 #include "test.h"
 
 void test() {
-    std::cout << "Fonction test appelée, v2, c'est la branche 2 la zonnne" << std::endl;
+    std::cout << "Fonction test appelée, v2, du 18avril!" << std::endl;
+
+    std::cout <<"J'espere le merge va marcher" << std::endl;
 }
